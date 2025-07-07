aa34021c2c35bbae0afda2b5d8929e4a040a3f72
afshankhatri <mohdafshankhatri@gmail.com>
2025-07-07 15:45:40 +0530

    all students are available with specific branch name and not th ebranch code

--- static/js/LCgenerator/LCgenerator.js
+++ static/js/LCgenerator/LCgenerator.js
@@ -3,7 +3,23 @@ function openModal(courseName, userId, branch, GR_no, srNo,LCgenerated) {
     // Populate modal content with the student's data
     document.getElementById("modalName").innerText = courseName;
     document.getElementById("modalEmail").innerText = userId;
-            document.getElementById("modalBranch").innerText = branch;  
+    if (branch == 0) {
+        document.getElementById("modalBranch").innerText = "Artificial Intelligence & Machine Learning";
+    } else if (branch == 1) {
+        document.getElementById("modalBranch").innerText = "IOT";
+    } else if (branch == 2) {
+        document.getElementById("modalBranch").innerText = "Computer Science";
+    } else if (branch == 3) {
+        document.getElementById("modalBranch").innerText = "IT";
+    } else if (branch == 4) {
+        document.getElementById("modalBranch").innerText = "EXTC";
+    } else if (branch == 5) {
+        document.getElementById("modalBranch").innerText = "Mech";
+    } else if (branch == 6) {
+        document.getElementById("modalBranch").innerText = "Civil";
+    } else {
+        document.getElementById("modalBranch").innerText = "Unknown Branch";
+    } 
     document.getElementById("modalGRNo").innerText = GR_no; 
     
     
